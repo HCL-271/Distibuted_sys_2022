5f1977d83aa5a55fd6700f1e7521eb4f4b29fc6e
H-Chlor <32020828+HCL-271@users.noreply.github.com>
2022-11-21 13:06:02 +0300

    Update SpinLocks.h

--- lab2/SpinLocks.h
+++ lab2/SpinLocks.h
@@ -11,6 +11,22 @@
 #define store_barrier()  __asm__ volatile("sfence" ::: "memory")
 
 #define spinloop_pause() __asm__ volatile("pause")
+/*
+It's _mm_pause() and a compile memory barrier wrapped into one GNU C Extended ASM statement. https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
+
+asm("" ::: "memory") prevents compile-time reordering of memory operations across it, like C++11 std::atomic_signal_fence(std::memory_order_seq_cst). 
+(not atomic_thread_fence; although on x86 preventing compile-time reordering is sufficient to make it an acquire + release fence because the only run-time reordering that x86 allows is StoreLoad.) See Jeff Preshing's Memory Ordering at Compile Time article.
+
+Making the asm instruction part non-empty also means those asm instructions will run every time the C logically runs that source line (because it's volatile).
+*/
+/*
+
+the "memory" part tells gcc that memory may have been modified inside the asm block.
+While it's not true (pause doesn't affect memory content), it's used as partial anti-reorder barrier for the compiler.
+Together with volatile keyword it's almost sure the pause will be at the end of the while-body 
+and nothing from the code ahead will be moved after the pause, actually it may even prevent the optimizer to unroll the loop (or at least make it less likely). 
+Which look as something what the author probably did want
+*/
 
 //------------------------------------------------------------------
 // TAS lock
